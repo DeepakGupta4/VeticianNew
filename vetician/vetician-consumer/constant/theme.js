071aa8e7dae51d012310f7ec7814290016fa0a80
Sher <sherali03012002@gmail.com>
2026-03-25 09:56:29 +0530

    fixing the issues of data

--- vetician/vetician-consumer/constant/theme.js
+++ vetician/vetician-consumer/constant/theme.js
@@ -11,7 +11,7 @@ export const COLORS2 = {
   bg: '#FAFAFA',
   card: '#FFFFFF',
   text: '#2D3A1F',
-  subtext: '#6B7B5E',
+  subtext: '#6B7B5E', 
   border: '#E8F0E1',
   accent: '#F1F8E9',
   shadow: '#C5D9A8',
